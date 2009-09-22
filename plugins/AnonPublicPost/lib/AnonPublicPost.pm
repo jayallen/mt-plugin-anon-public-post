4fb0890d99bd2d791a899aa4cd2b1af5dcabb851
Jay Allen <jay@endevver.com>
2009-09-22 00:15:08 -0700

    Now forcibly adding cookie onto app->{cookies} hash because something about Byrne's setup was preventing the newly set cookie from being seen

--- plugins/AnonPublicPost/lib/AnonPublicPost.pm
+++ plugins/AnonPublicPost/lib/AnonPublicPost.pm
@@ -3,14 +3,12 @@ package AnonPublicPost;
 use strict;
 use warnings;
 
-# use MT::Log::Log4perl qw( l4mtdump );
-# our $logger;
+# use MT::Log::Log4perl qw( l4mtdump ); our $logger;
 
 sub check_user {
     my $cb    = shift;
     my $app   = shift;
-    # $logger ||= MT::Log::Log4perl->new();
-    # $logger->trace();
+    # $logger ||= MT::Log::Log4perl->new(); $logger->trace();
 
     # We only run if we detect an anonymous user posting an entry
     return if $app->user or ($app->mode || '') ne 'post';
@@ -24,21 +22,22 @@ sub check_user {
 
     # $logger->debug('$user: ', l4mtdump($user));
 
-    $app->start_session( $user, 0 );
+    my $cookieparam = $app->start_session( $user, 0 );
+    require CGI::Cookie;
+    $app->{cookies}{$app->user_cookie} = CGI::Cookie->new( %$cookieparam );
+    # $logger->debug('$app->{cookies}: ', l4mtdump($app->{cookies}));
+
     my $session = $app->{session};
-
     $app->param( 'magic_token', $session->id );
-    # $logger->debug('$app->param(magic_token): ', $app->param('magic_token'));
     $app->user( $user );
-
+    # $logger->debug('$app->param(magic_token): ', $app->param('magic_token'));
     $app->request('kill_anonymous_session', 1);
 }
 
 sub reap_session {
     my ($cb) = shift;
     my $app = MT->instance;
-    # $logger ||= MT::Log::Log4perl->new();
-    # $logger->trace();
+    # $logger ||= MT::Log::Log4perl->new(); $logger->trace();
     
     if ( $app->request('kill_anonymous_session') ) {
         # $logger->debug('Killing session: ', $app->{session}->id);
